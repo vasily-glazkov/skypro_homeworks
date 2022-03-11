d432818364c65d23971ffb64077136da9e6b539b
vasily-glazkov <vglazkoff@gmail.com>
2022-03-11 12:13:48 +0200

    Updated variables names for more concise meaning

--- 10_2/main.py
+++ 10_2/main.py
@@ -15,19 +15,19 @@ def index_page():
 	return display_string
 
 
-@app.route("/candidate/<int:x>/")
-def candidate_page(x):
+@app.route("/candidate/<int:id_>/")
+def candidate_page(id_):
 	for i in data:
-		if i['id'] == x:
+		if i['id'] == id_:
 			return f"<img src='https://picsum.photos/200'>\n" \
 				f"<pre>{i['name']}-\n{i['position']}\n{i['skills']}</pre>"
 
 
-@app.route("/skill/<x>/")
-def search_skill(x):
+@app.route("/skill/<skill>/")
+def search_skill(skill):
 	display_string = ""
 	for i in data:
-		if x.lower() in i['skills'] or x in i['skills']:
+		if skill.lower() in i['skills'] or skill in i['skills']:
 			display_string += f"<pre>{i['name']}-\n{i['position']}\n{i['skills']}</pre>"
 	return display_string
 
